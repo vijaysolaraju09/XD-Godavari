5651e8956dc3dad03dba4ed53f475e53a638dc59
vijaysolaraju09 <vijaysolaraju9848@gmail.com>
2022-06-21 15:17:46 +0530

    commit

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+$().ready(function(){
+    $('.slick-carousel').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+      
+  });
